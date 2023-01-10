b13f3d39061a8ba778d77f5b6d85300147ef4471
17thummer <17thummer@gmail.com>
2023-01-09 23:27:35 -0600

    Create the robot blocks and build joints

--- generate.py
+++ generate.py
@@ -1,13 +1,56 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2.5,2.5,z], size=[length, width, height])
+    pyrosim.End()
+    return
+
+def Create_Robot_Practice():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link0_Link1", parent = "Link0", child = "Link1", type = "revolute", position = [0,0,1.0])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link1_Link2", parent = "Link1", child = "Link2", type = "revolute", position = [0,0,1.0])
+    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link2_Link3", parent = "Link2", child = "Link3", type = "revolute", position = [0,0.5,0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link3_Link4", parent = "Link3", child = "Link4", type = "revolute", position = [0,1.0,0])
+    pyrosim.Send_Cube(name="Link4", pos=[0,0.5,0], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link4_Link5", parent = "Link4", child = "Link5", type = "revolute", position = [0,0.5,-0.5])
+    pyrosim.Send_Cube(name="Link5", pos=[0,0,-0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link5_Link6", parent = "Link5", child = "Link6", type = "revolute", position = [0,0,-1.0])
+    pyrosim.Send_Cube(name="Link6", pos=[0,0,-0.5], size=[length, width, height])
+    pyrosim.End()
+    return ()
+
+def Create_Robot_Trips():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link0_Link1", parent = "Link0", child = "Link1", type = "revolute", position = [0.5,0,1.0])
+    pyrosim.Send_Cube(name="Link1", pos=[0.5,0,0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link1_Link2", parent = "Link1", child = "Link2", type = "revolute", position = [1.0,0,0])
+    pyrosim.Send_Cube(name="Link2", pos=[0.5,0,-0.5], size=[length, width, height])
+    pyrosim.End()
+    return()
 
 length = 1
 width = 1
 height = 1
 
+x = 0
+y = 0
+z = 0.5
 
-for i in range (5):
+Create_World()
+Create_Robot_Trips()
+#Create_Robot_Practice()  #This was the step by step tutorial from the course
+
+
+
+# This section builds the tower of blocks for Section D
+'''for i in range (5):
     for j in range (5):
         length = 1
         width = 1
@@ -27,5 +70,5 @@ for i in range (5):
             print("i = ",i)
             print("j = ",j)
             print("k = ",k)
+'''
 
-pyrosim.End()
