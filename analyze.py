79e09562d8a40a2756c8d15c947e4a5beb3063e4
17thummer <17thummer@gmail.com>
2023-01-15 22:48:07 -0600

    Added Second Leg Sensor

--- analyze.py
+++ analyze.py
@@ -1,4 +1,15 @@
-import numpy
-import matplotlib.pyplot 
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+backLegSensorValues = np.load('data/backLeg.npy')
+frontLegSensorValues = np.load('data/frontLeg.npy')
+
+print(backLegSensorValues)
+print(frontLegSensorValues)
+
+plt.plot(backLegSensorValues, label='Back Leg', linewidth = '4')
+plt.plot(frontLegSensorValues, label='Front Leg', linewidth = '2')
+
+plt.legend()
+plt.show()
--- simulate.py
+++ simulate.py
@@ -16,15 +16,20 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
 
 i = 1
-for i in range(1000):
+for i in range(100):
     p.stepSimulation()  #comment this out to see the starting configuration of the blocks
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
 print(backLegSensorValues)
+print(frontLegSensorValues)
+
 numpy.save('data/backLeg', backLegSensorValues)
+numpy.save('data/frontLeg', frontLegSensorValues)
 p.disconnect()
 
 #comment
