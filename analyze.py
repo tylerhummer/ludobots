1fd82ea7d9640e5ffb842e0309dff0e1eb2157d9
17thummer <17thummer@gmail.com>
2023-01-16 19:10:56 -0600

    sinewave motor input completed sway

--- analyze.py
+++ analyze.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 backLegSensorValues = np.load('data/backLeg.npy')
 frontLegSensorValues = np.load('data/frontLeg.npy')
 targetAngles = np.load('data/targetAngles.npy')
-
+'''
 print(backLegSensorValues)
 print(frontLegSensorValues)
 
@@ -13,7 +13,7 @@ plt.figure(1)
 plt.plot(backLegSensorValues, label='Back Leg', linewidth = '4')
 plt.plot(frontLegSensorValues, label='Front Leg', linewidth = '2')
 plt.legend()
-
+'''
 
 plt.figure(2)
 plt.plot(targetAngles, label="Sine Function", linewidth='3')
--- simulate.py
+++ simulate.py
@@ -17,27 +17,27 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-length_sim = 500
+length_sim = 1000
 backLegSensorValues = numpy.zeros(length_sim)
 frontLegSensorValues = numpy.zeros(length_sim)
 
 target = numpy.pi/2.0
-targetAngles = numpy.sin(numpy.linspace(0, 2*numpy.pi, length_sim))
+targetAngles = (numpy.pi/4)*(numpy.sin(numpy.linspace(0, 2*numpy.pi, length_sim)))
 
 i = 1
 for i in range(length_sim):
     p.stepSimulation()  #comment this out to see the starting configuration of the blocks
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform((-numpy.pi/2.0),(numpy.pi/2.0)), maxForce=50)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform((-numpy.pi/2.0),(numpy.pi/2.0)), maxForce=50)
-    time.sleep(1/50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce=50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = targetAngles[i], maxForce=50)
+    time.sleep(1/100)
 print(backLegSensorValues)
 print(frontLegSensorValues)
 
 numpy.save('data/backLeg', backLegSensorValues)
 numpy.save('data/frontLeg', frontLegSensorValues)
-numpy.save('data/targetAngles', targetAngles)
+#numpy.save('data/targetAngles', targetAngles)
 p.disconnect()
 
 #comment
