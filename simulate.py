d008a89a2efd8582388ef179ac608bea18bac969
17thummer <17thummer@gmail.com>
2023-01-16 18:33:36 -0600

    randomize target position and reduce force

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
 import numpy
+import random
 
 
 
@@ -20,16 +21,16 @@ length_sim = 500
 backLegSensorValues = numpy.zeros(length_sim)
 frontLegSensorValues = numpy.zeros(length_sim)
 
-target = numpy.pi/4
+target = numpy.pi/2.0
 
 i = 1
 for i in range(length_sim):
     p.stepSimulation()  #comment this out to see the starting configuration of the blocks
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = target, maxForce=500)
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = -target, maxForce=500)
-    time.sleep(1/100)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform((-numpy.pi/2.0),(numpy.pi/2.0)), maxForce=50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_FrontLeg', controlMode = p.POSITION_CONTROL, targetPosition = random.uniform((-numpy.pi/2.0),(numpy.pi/2.0)), maxForce=50)
+    time.sleep(1/50)
 print(backLegSensorValues)
 print(frontLegSensorValues)
 
