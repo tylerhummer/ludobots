b4d135268249e833283adb95dbcac9d0d494a452
17thummer <17thummer@gmail.com>
2023-01-16 18:12:58 -0600

    change the target angle of the joints

--- simulate.py
+++ simulate.py
@@ -20,12 +20,14 @@ length_sim = 500
 backLegSensorValues = numpy.zeros(length_sim)
 frontLegSensorValues = numpy.zeros(length_sim)
 
+target = numpy.pi/4
+
 i = 1
 for i in range(length_sim):
     p.stepSimulation()  #comment this out to see the starting configuration of the blocks
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce=500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName ='Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = target, maxForce=500)
     time.sleep(1/60)
 print(backLegSensorValues)
 print(frontLegSensorValues)
