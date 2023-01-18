d2197fef6cc22e24d0e7d83dd267edb654f7b0e4
17thummer <17thummer@gmail.com>
2023-01-18 11:40:40 -0600

    moved deconstructor

--- simulation.py
+++ simulation.py
@@ -1,13 +1,19 @@
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+from world import WORLD
+from robot import ROBOT
 
 class SIMULATION:
     def __init__(self):
         self.physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
-        pyrosim.Prepare_To_Simulate(robotId)
+        #pyrosim.Prepare_To_Simulate(robotId)
 
         self.world = WORLD()
         self.robot = ROBOT()
+
+
+    def __del__(self):
+        p.disconnect()
--- world.py
+++ world.py
@@ -1,3 +1,5 @@
+import pybullet as p
+
 class WORLD:
     def __init__(self):
         p.loadSDF("world.sdf")
