b2c0013d961f4b75f302a5672320ac5b045c32ca
17thummer <17thummer@gmail.com>
2023-01-26 15:05:48 -0600

    finish creating the hill climber

--- search.py
+++ search.py
@@ -1,5 +1,11 @@
 import os 
+from hillclimber import HILL_CLIMBER
 
+
+hc = HILL_CLIMBER()
+
+'''
 for i in range(5):
     os.system("python generate.py")
     os.system("python simulate.py")
+'''
---
+++ solution.py
@@ -0,0 +1,10 @@
+import numpy
+
+
+class SOLUTION:
+    
+    def __init__ (self):
+        self.weights = numpy.random.rand(3,2)
+        self.weights = self.weights * 2 - 1
+        
+        
