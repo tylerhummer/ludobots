73e0f6a7d40201ece47e25ff269635994b8daef9
17thummer <17thummer@gmail.com>
2023-01-31 16:33:21 -0600

    create parallel hill climber
    program still runs with normal hill climber functionality

---
+++ parallelHillClimber.py
@@ -0,0 +1,47 @@
+from solution import SOLUTION 
+from simulation import SIMULATION
+import constants as c
+import copy
+
+class PARALLEL_HILL_CLIMBER:
+    
+    def __init__ (self):
+        self.parent = SOLUTION()
+
+    
+    def Evolve(self):
+        self.parent.Evaluate("GUI")
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+            #print('current generation ', currentGeneration)
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+        
+
+    def Mutate(self):
+        self.child.Mutate()
+       
+
+    def Select(self):
+        #print('parent fitness ', self.parent.fitness)
+        if (self.child.fitness < self.parent.fitness):
+            self.parent = self.child
+
+
+    def Print(self):
+        print()
+        print()
+        print('parent fitness ', self.parent.fitness, ', child fitness ', self.child.fitness)
+        print()
+        print()
+
+    def Show_Best(self):
+        self.parent.Evaluate("GUI")
--- search.py
+++ search.py
@@ -1,10 +1,10 @@
 import os 
-from hillclimber import HILL_CLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
 
-hc = HILL_CLIMBER()
-hc.Evolve()
-hc.Show_Best()
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
 
 '''
 for i in range(5):
