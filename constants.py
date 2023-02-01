32e58b3c6d7ab44a3b34fd2b40624483207a5a06
17thummer <17thummer@gmail.com>
2023-02-01 02:12:41 -0600

    finish quadruped

--- constants.py
+++ constants.py
@@ -22,8 +22,10 @@ x_vals = numpy.linspace(0, 2*numpy.pi, length_sim)
 backLeg_targetAngles = (backLeg_amplitude)*(numpy.sin(backLeg_frequency * x_vals + backLeg_phaseOffset))
 frontLeg_targetAngles = (frontLeg_amplitude)*(numpy.sin(frontLeg_frequency * x_vals + frontLeg_phaseOffset))
 
-numberOfGenerations = 1
-populationSize = 1
+motorJointRange = 0.2
+
+numberOfGenerations = 10
+populationSize = 10
 
 numSensorNeurons = 9
 numMotorNeurons = 8
