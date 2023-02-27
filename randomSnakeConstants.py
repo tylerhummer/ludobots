31b77f5bc9f03d555a0cc7235cd86e2c6848b2ec
17thummer <17thummer@gmail.com>
2023-02-27 14:51:24 -0600

    Ready to begin simulations for seed 5

--- randomSnakeConstants.py
+++ randomSnakeConstants.py
@@ -8,14 +8,14 @@ length_sim = 1000
 
 motorJointRange = 0.3
 
-numberOfGenerations = 5
-populationSize = 5
+numberOfGenerations = 1
+populationSize = 1
 
 numLinks = 10
 numSensorNeurons = 5
 numMotorNeurons = numLinks - 1
 
-seed_number = 5
+seed_number = 7760
 checkpoint_file_name = "checkpoint_seed" + str(seed_number) + ".pkl"
 csv_file_name = "seed_" + str(seed_number) + ".csv"
 
